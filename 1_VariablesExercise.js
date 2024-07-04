84ad82f8f5c6c1252a8c6eb0574229b30752e72e
Thane Chaves <tmoonder@gmail.com>
2024-07-04 01:40:22 -0400

    Added and completed a file for variable exercises.

---
+++ 1_VariablesExercise.js
@@ -0,0 +1,114 @@
+
+// Exercise 1. Simply declare a variable named 'emptyVariable'
+
+let emptyVariable;
+
+// Exercise 2. Declare a variable called 'myName'
+
+let myName;
+
+// Exercise 3. Assign your name as the value for 'myName'
+
+myName = "Thane";
+
+// Exercise 4. Declare a variable called dreamDestination and give it the value
+// of what your dream vacation destination is.
+
+let dreamDestination = "New Orleans";
+
+// Exercise 5. Make the following variables and assignments
+// - a variable called num1 with a value greater than 1 and less than 10
+// - a variable called num2 with a value of greater than or equal to 1 and less
+// than or equal to 10
+
+let num1 = 6;
+let num2 = 3;
+
+// Exercise 6. Write a comment below explaining the difference between the rules
+// for num1 and num2 from Exercise 5. What numbers would be valid values for
+// num2 but not num1?
+
+// For num1, a value greater than 1 and less than 10 necessarily excludes both 1
+// and 10, meaning the valid numbers would be 2, 3, 4, 5, 6, 7, 8, and 9.
+// For num2, a value greater than or equal to 1 and less than or equal to 10
+// includes both numbers, meaning the valid numbers would be 1, 2, 3, 4, 5, 6,
+// 7, 8, 9, and 10.
+
+// Exercise 7. Now we will try some addition.
+// a. Assign the the values of 4 and 6 to num1 and num2, respectively.
+// b. Make a new variable called 'theSum', and use 'num1' and
+// 'num2' to assign its value using the "+" operator.
+
+num1 = 4;
+num2 = 6;
+let theSum = num1 + num2;
+
+// Exercise 8. Now we will try some multiplication.
+// Make a new variable called 'theProduct', multiply num1 and num2 and assign
+// the resulting value to 'theProduct'.
+
+let theProduct = num1 * num2;
+
+// Exercise 9.
+// Make a new variable called 'statement' and using 'myName' and
+// 'dreamDestination' and the concatenation method of your choice, make the
+// value of statement to be:
+// "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
+
+let statement = `Hi, my name is ${myName}, and I can't wait to visit ${dreamDestination}!`;
+
+// Exercise 10. What is the data type of the value stored in 'dreamDestination',
+// place your answer in the quotes below:
+
+const myAnswerForExercise10 = "string";
+
+// Exercise 11. What is the data type of the value stored in 'product',
+// place your answer in the quotes below:
+
+const myAnswerForExercise11 = "number";
+
+// Exercise 12. What do you think the value of emptyVariable is?
+// Place your answer below:
+
+const myAnswerForExercise12 = "undefined";
+
+// Exercise 13. If
+const A = "R";
+const B = 1;
+const C = 4;
+const D = "D";
+
+// What is the value of B + C
+const myAnswerForExercise13 = 5;
+
+// Exercise 14 What is the value of
+// A + "n" + D
+const myAnswerForExercise14 = "RnD";
+
+// Exercise 15 What is the value of
+// A + B * 2 + D + C / 2:
+const myAnswerForExercise15 = "R2D2";
+
+// Exercise 16 What is the value of A * B
+const myAnswerForExercise16 = NaN;
+
+// Exercise 17 What is the value of true || false
+const myAnswerForExercise17 = true;
+
+// Exercise 18 What is the value of 1 === "1"
+const myAnswerForExercise18 = false;
+
+// Exercise 19 What is the value of 1 == "1"
+const myAnswerForExercise19 = true;
+
+// Exercise 20 What is the value of 1 < 1
+const myAnswerForExercise20 = false;
+
+// Exercise 21 What is the value of 1 < 2 < 3
+const myAnswerForExercise21 = true;
+
+// Exercise 22 What is the value of 2 < 1 < 3
+const myAnswerForExercise22 = true;
+
+// I was familiar with most of this, but needed some help from the chrome 
+// console to remember how certain data types are evaluated in Javascript.
